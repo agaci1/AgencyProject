2813243b49a440969d6fdbc732e2a09b40979c70
alkeogaci <keogaci@gmail.com>
2025-07-01 03:18:38 +0200

    Cleanup: fix folder structure, remove duplicates

--- agency.backend/agency.backend/backend/src/main/java/com/agency/backend/repository/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.agency.backend.repository;
-
-import com.agency.backend.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
-
